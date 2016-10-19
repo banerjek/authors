322ec9072e665d6652c3ebb7f1a4d48a58e87f68
Kyle Banerjee <kyle.banerjee@gmail.com>
2016-10-19 15:35:27 -0700

    Separated author and organizational ID handling. Improved field extraction

--- author.pl
+++ author.pl
@@ -9,7 +9,8 @@ my $outfile = 'author-data.txt';
 #####################################
 # store some regexes to get key fields
 my $ohsu_regex = qr/<owl:Class rdf:about="http:\/\/ohsu-metadata\/([^"]*)">/; 
-my $label_scopus_regex = qr/<rdfs:label rdf:datatype="&xsd;string">([^_]*)_([0-9]*)</; 
+my $label_regex = qr/<rdfs:label rdf:datatype="&xsd;string">([^_]*)[_<]/; 
+my $scopus_regex = qr/<ohsu-metadata3:hasIdentifier rdf:datatype="&xsd;string">ScopusAuthor:([0-9]*)</; 
 my $member_of_regex = qr/<rdfs:subClassOf rdf:resource="http:\/\/ohsu-metadata\/([^"]*)"/; 
 my $detectunit_regex = qr/ohsu-metadata3:hasName rdf:/; 
 my $endrecord_regex = qr/<\/owl:Class>/; 
@@ -24,11 +25,13 @@ my $isunit = 0;
 my $record = '';
 my $ohsu_id = '';
 my $scopus_id = '';
+my $record_id = '';
 my $member_of = '';
 my $label = '';
 my $line = '';
 
 my @source_data = [];
+my @memberships = [];
 
 local $/ = undef;
 $/ = "\n";
@@ -38,31 +41,56 @@ open (INFILE, $infile) or die("Unable to open data file \"$infile\"");
 close $infile;
 
 foreach $line(@source_data) {
+	#####################
+	# Get OHSU identifier
+	#####################
 	if ($line =~ /$ohsu_regex/) {
 		$ohsu_id = $1;
 		}
+	if ($line =~ /$label_regex/) {
+		$label = $1;
+		}
+	if ($line =~ /$scopus_regex/) {
+		$scopus_id = $1;
+		$record_id = $scopus_id;
+		}
+
 	if ($line =~ /$detectunit_regex/) {
 		$isunit = 1;	
+		$record_id = $ohsu_id;
 		}
 	if ($line =~ /$member_of_regex/) {
 		$member_of = $1;	
-		#push @{$organization_members{$member_of}}, $ohsu_id;
-		push @{$organization_members{$member_of}}, "$scopus_id $label";
-		}
-	if ($line =~ /$label_scopus_regex/) {
-		$scopus_id = $2;
-		$label = $1;
-		$all_names{$ohsu_id} = $label;
+		###############################################
+		# keep a list of memberships until we know this 
+		# record is for a person or unit
+		###############################################
+		push @memberships, $ohsu_id;
 		}
 
 	##########################################
 	# Detect end of record and reset variables
 	##########################################
 	if ($line =~ /$endrecord_regex/) {
+		##################
+		# Record the label
+		##################
+		$all_names{$record_id} = $label;
+		###################################################
+		# Add memberships
+		#
+		# People and units are stored in same hash but 
+		# are treated differently based on ID pattern match
+		###################################################
+		foreach my $membership(@memberships) {
+			push @{$organization_members{$member_of}}, $record_id;
+			}
 		$scopus_id = '';
 		$ohsu_id = '';
+		$record_id = '';
 		$label = '';
 		$isunit = 0;
+		@memberships = [];
 		}
 	}
 
@@ -71,8 +99,7 @@ foreach $line(@source_data) {
 	######################
 	
 	for my $org_key(keys %organization_members) {
-		print "$all_names{$org_key}\n";
-		my $members = join(':', @{$organization_members{$org_key}});
+		print "$all_names{$org_key} ----- $org_key\n";
+		my $members = join("\n", @{$organization_members{$org_key}});
 		print "$members\n";
-
 		}
